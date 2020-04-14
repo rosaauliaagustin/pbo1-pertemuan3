1e64033c1e064c863ca4298e6c5447a18111c5ab
rosaauliaagustin <rosaauliaagustin22@gmail.com>
2020-04-14 20:49:08 +0700

    Switch jalanjalan

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,25 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case 1:
+                System.out.println("uluh lapas");
+                break;
+            case 2:
+                System.out.println("uluh pacaran");
+                break;
+            case 3:
+                System.out.println("obat nyamuk");
+                break;
+            case 4:
+                System.out.println("double date");
+                break;
+            case 5:
+                System.out.println("makhlus kurik");
+                break;
+
+        }
+    }
+}
